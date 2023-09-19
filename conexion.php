e5bdf70e37473dfe735ea42d06ec2758affa2a29
EMaguinaRicra <eloy.maguinaricra@gmail.com>
2023-09-19 16:52:31 -0500

    Corrección de conexion.php para Cpanel

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; // Nombre del servidor de la base de datos
-$username = "root"; // Nombre de usuario para acceder a la base de datos - El usuario para la base de datos en cPanel es ingenie5_ingenieriasidney
-$password = ""; // Contraseña para acceder a la base de datos - La contraseña para cPanel "yxYiw7@KtsmkBM7"
-$dbname = "ingenieriasidney"; // Nombre de la base de datos - El nombre de la base de datos en cPanel es ingenie5_ingenieriasidney
+$username = "ingenie5_ingenieriasidney"; // Nombre de usuario para acceder a la base de datos - El usuario para la base de datos en cPanel es ingenie5_ingenieriasidney
+$password = "yxYiw7@KtsmkBM7"; // Contraseña para acceder a la base de datos - La contraseña para cPanel "yxYiw7@KtsmkBM7"
+$dbname = "ingenie5_ingenieriasidney"; // Nombre de la base de datos - El nombre de la base de datos en cPanel es ingenie5_ingenieriasidney
 
 // Crear una conexión
 $conn = mysqli_connect($servername, $username, $password, $dbname);
