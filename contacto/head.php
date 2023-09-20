000c6a7c3c450b2e977bdba3a024dad1077e28d8
EMaguinaRicra <eloy.maguinaricra@gmail.com>
2023-09-19 20:33:53 -0500

    Modificación de la etiqueta head en cada página

--- contacto/head.php
+++ contacto/head.php
@@ -2,7 +2,9 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="Promina - Construction And Building Template">
+  <meta name="description" content="Especializados en soluciones de ingeniería civil y arquitectura sostenibles. 
+        Nuestro equipo resuelve desafíos de diseño y construcción con enfoque en la eficiencia energética, el 
+        respeto al medio ambiente y la innovación en cada aspecto del proyecto. Descubre cómo podemos hacer que tu proyecto se haga realidad.">
   <link href="../assets/images/favicon/favicon.png" rel="icon">
   <title>Contacto</title>
   <link rel="stylesheet"
